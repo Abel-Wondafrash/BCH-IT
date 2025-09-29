633bebbde2d7010282639bddc5b29314034d3172
Abel Wondafrash <abel.wondafrash@gmail.com>
2025-09-29 06:48:05 +0300

    [ADD] sale_custom_readonly: Make price_unit field readonly in all sale order line views

---
+++ Systems and Tools/ERP/Odoo 11/Customizations/Custom Modules/sale_custom_readonly/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    'name': 'Sale Custom Readonly',
+    'version': '11.0.1.0.0',
+    'summary': 'Make price_unit readonly in Sale Order Lines',
+    'depends': ['sale'],
+    'data': [
+        'views/sale_order_views.xml',
+    ],
+    'installable': True,
+    'application': False,
+}
